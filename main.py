48bd299fbf146d997513e6c9e60397192f89af11
PETER MUYOMBA <122818012+Simonmuyomba@users.noreply.github.com>
2023-01-16 19:51:27 +0000

    Understanding of the Input & len() function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # Write your code below this line 👇
 print("Hello Peter\n Muyomba \n You've got this 🤗")
 print("Hello" + " " + "Muyomba")
+#print function completed
+
+#input function
+print("Hello "+ input('What is your name?') + '!')
+#input function calculating the length of characters
+
+print(len(input('What is your name? ')))
